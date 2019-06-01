44e06f7a8983a82664a2db90ca21c218eb36b923
Greg Allen <greg@firstandthird.com>
2019-06-01 12:27:30 -0700

    fix path

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const aug = require('aug');
 const confi = require('confi-lite');
-const config = confi('@architect/shared/conf');
+
+const confPath = process.env.SHARED_PATH || path.dirname(require.resolve('@architect/shared/conf/default.json'));
+const config = confi(confPath);
 const logrAll = require('logr-all');
 const cache = require('@firstandthird/memory-cache');
 
