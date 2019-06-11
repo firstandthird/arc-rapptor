44b54e14605197de62bd91a5fba3917dd7608147
orthagonal <orthagonal1@gmail.com>
2019-06-11 22:46:46 +0000

    test what remains

--- test/test.rapptor.js
+++ test/test.rapptor.js
@@ -1,33 +1,30 @@
 const tap = require('tap');
-const nunjucks = require('nunjucks');
 
 tap.test('rapptor', async t => {
-  const { config, log, cache, aug } = require('../');
-  t.equal(typeof log, 'function');
-  // init nunjucks:
-  const env = nunjucks.configure(`${__dirname}/views`, { autoescape: true });
-  env.addFilter('shorten', (str, count) => str.slice(0, count || 5));
-  let count = 0;
-  // // declare a render function:
-  const render = function(request) {
-    const forceUpdate = (request.query.update === '1');
-    return cache.memo('home', () => {
-      count++;
-      log(['pagedata'], 'getting slugs');
-      const html = nunjucks.render('test.njk', {
-        value: 17
-      });
-      return html;
-    }, config.viewCacheDuration, forceUpdate);
+  process.env.SHARED_PATH = __dirname;
+  const { config, log, aug, logRequest } = require('../');
+  t.equal(typeof log, 'function', 'exports a log function');
+  log(['pagedata'], 'getting slugs');
+  t.match(aug({ val1: 1 }, { val2: 3 }, { val2: 2, val3: 3 }), {
+    val1: 1,
+    val2: 2,
+    val3: 3
+  }, 'exports an aug (object deep-clone) function');
+  t.ok(config.json, 'loads and exports the config');
+  const oldLog = console.log;
+  let val = '';
+  console.log = (param) => {
+    val = param;
   };
-  const request = {
-    query: {}
-  };
-  let result = await render(request);
-  t.match(result, 'The value is 17');
-  t.equal(count, 1);
-  result = await render(request);
-  t.match(result, 'The value is 17');
-  t.equal(count, 1);
+  logRequest({
+    method: 'get',
+    path: '/',
+    query: {
+      'what-is-the-fastest-land-animal': 'the cheetah'
+    }
+  });
+  console.log = oldLog;
+  t.equal(val, 'level=INFO msg="get /" tag="request" path="/" query.what-is-the-fastest-land-animal="the cheetah"',
+    'exports a logRequest function that logs incoming request objects');
   t.end();
 });
