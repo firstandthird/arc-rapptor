9f2362985380796ced77c6c27e39a88fa6d198aa
orthagonal <orthagonal1@gmail.com>
2019-10-23 21:50:19 +0000

    normalize

--- test/test.rapptor.js
+++ test/test.rapptor.js
@@ -151,3 +151,25 @@ tap.test('response method redirectTrailingSlash', async t => {
   }, 'redirectTrailingSlash can be set to false');
   t.end();
 });
+
+tap.test('response method normalizes req and response', async t => {
+  process.env.SHARED_PATH = __dirname;
+  const { reply, response } = require('../');
+  const handler = response((req) => {
+    t.match(req.headers['content-type'], 'application/json', 'headers normalized as lowercase');
+    t.match(req.queryStringParameters, {}, 'req.queryStringParameters is always present');
+    t.match(req.pathParameters, {}, 'req.pathParameters is always present');
+    t.match(req.body, { isJson: true }, 'JSON string is always converted to object');
+    return reply.json({ okay: true });
+  });
+  const response1 = await handler({
+    path: '/api',
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: '{ "isJson": true }'
+  });
+  t.match(response1.headers, { 'content-type': 'application/json; charset=utf8' });
+  t.end();
+});
